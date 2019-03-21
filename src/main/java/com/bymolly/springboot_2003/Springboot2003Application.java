b4d29ce0985ae5997f017ea76fbdb3ff9856326e
GBTC440007UR <GBTC440007UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-20 21:00:18 -0400

    Initial commit

---
+++ src/main/java/com/bymolly/springboot_2003/Springboot2003Application.java
@@ -0,0 +1,13 @@
+package com.bymolly.springboot_2003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2003Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bymolly/springboot_2003/Springboot2003ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bymolly.springboot_2003;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot2003ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
